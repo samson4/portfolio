6f7d12974c933b9e74b1c44ad3443495777fa11f
samson4 <samsonkebede4@gmail.com>
2023-08-18 14:34:47 +0300

    added overflow hidden

--- components/PortfolioFilter.vue
+++ components/PortfolioFilter.vue
@@ -1,11 +1,11 @@
 <template >
-     <div class="portfolio-filter w-full">
+    <div class="portfolio-filter w-full">
         <div class="portfolio-head flex center">
-                <div>
-                    <ul>
-                        <li><a href="#header" class="hover:opacity-25">All</a></li>
-                    </ul>
-                </div>
+            <div>
+                <ul>
+                    <li><a href="#header" class="hover:opacity-25">All</a></li>
+                </ul>
+            </div>
             <div>
                 <ul>
                     <li> <a href="#portfolio">Cisco</a> </li>
@@ -13,53 +13,59 @@
             </div>
             <div>
                 <ul>
-                    <li><RouterLink to="#">Awards</RouterLink> </li>
+                    <li>
+                        <RouterLink to="#">Awards</RouterLink>
+                    </li>
                 </ul>
-                
+
             </div>
             <div>
                 <ul>
-                    <li><RouterLink to="#">Trend Micro</RouterLink></li>
+                    <li>
+                        <RouterLink to="#">Trend Micro</RouterLink>
+                    </li>
                 </ul>
-                
+
             </div>
             <div>
                 <ul>
-                    <li><RouterLink to="#">Certifications</RouterLink></li>
+                    <li>
+                        <RouterLink to="#">Certifications</RouterLink>
+                    </li>
                 </ul>
-                
+
             </div>
-           
-            
-           
+
+
+
         </div>
         <div class="portfolio-list flex my-12 max-w-[1200px] m-auto flex-wrap justify-around relative z-0">
             <div class="inner-portfolio-list mx-[16px] " v-for="cert in certs">
                 <ul>
-                    <div class="cert mb-6 relative z-0">
-                        <li><img class="hover:scale-125 transition-all" :src="cert.img" alt=""></li>
+                    <div class="cert mb-6 aspect-square relative overflow-hidden">
+                        <li><img class="hover:scale-105 transition-all" :src="cert.img" alt=""></li>
                     </div>
                 </ul>
             </div>
         </div>
+    </div>
+    <div>
+        <div class="float-right relative z-10  mt-[-160px]  ">
+            <img src="/2.png" class="" alt="">
         </div>
-        <div>
-            <div class="float-right relative z-10  mt-[-160px]  ">
-                <img src="/2.png" class="" alt="">
-            </div>
-        </div>
+    </div>
 </template>
 
 
 <script setup>
 const certs = ref([
-    {"name":"Cisco introduction to Networks","img":"/intro.png"},
-    {"name":"CCNA Enterprise Network Security and Automation","img":"/ENSA.png"},
-    {"name":"CCNA Switching Routing & Wireless Essentials","img":"/SRWE.png"},
-    {"name":"Trend Micro TrendCampus Foundation","img":"/TC.png"},
-    {"name":"Cisco introduction to Networks","img":"/AWSudemy.jpg"},
-    {"name":"Cisco introduction to Networks","img":"/intro.png"},
-    
+    { "name": "Cisco introduction to Networks", "img": "/intro.png" },
+    { "name": "CCNA Enterprise Network Security and Automation", "img": "/ENSA.png" },
+    { "name": "CCNA Switching Routing & Wireless Essentials", "img": "/SRWE.png" },
+    { "name": "Trend Micro TrendCampus Foundation", "img": "/TC.png" },
+    { "name": "Cisco introduction to Networks", "img": "/AWSudemy.jpg" },
+    { "name": "Cisco introduction to Networks", "img": "/intro.png" },
+
 
 ])
 
@@ -68,19 +74,22 @@ const certs = ref([
 </script>
 
 <style>
-.router-link-exacy-active{
+.router-link-exacy-active {
     color: #fb923c;
 }
-.portfolio-head{
+
+.portfolio-head {
     text-align: center;
     justify-content: space-evenly;
     /* max-width: 80%; */
-   
+
 }
-ul li{
+
+ul li {
     font-size: 18px;
 }
-.cert li img{
+
+.cert li img {
     /* background-color: aliceblue;......''''[] */
     max-width: 360px;
     max-height: 350px;
