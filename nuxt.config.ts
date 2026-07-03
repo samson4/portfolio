c14701fd092205c5dd8800606ac2473b551cfbcd
orbit <samsonkebede4@gmail.com>
2026-07-03 10:07:23 +0300

    CMS Based portfolio

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  compatibilityDate: '2026-07-02',
   devtools: { enabled: true },
+  
   modules:[
     '@nuxtjs/tailwindcss',
-    'nuxt-icon'
-
-  ]
+    'nuxt-icon',
+    '@nuxt/content',
+    'nuxt-component-meta',
+    'nuxt-studio'
+  ],
+  studio: {
+    repository: {
+      provider: 'github', // 'github' or 'gitlab'
+      owner: 'samson4',
+      repo: 'portfolio',
+      branch: 'main'
+    }
+  }
 })
